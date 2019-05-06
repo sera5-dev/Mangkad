fa82a4c5dba9079531cc639640e828d0df99d775
user <guungofficial@gmail.com>
2019-05-06 10:54:02 +0700

    first commit

---
+++ src/mangkad/models/DestWisata.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mangkad.models;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user
+ */
+public class DestWisata {
+    
+    private String name;
+    private ArrayList<TempatWisata> tempatWisata;
+    
+    public DestWisata(String name) {
+        
+    }
+    
+    public class TempatWisata {
+        
+    }
+}
